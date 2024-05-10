67be7e478be7d497819aa7fd19ad48a75f5269df
Sw1ftFox <v.suvorkin2017@gmail.com>
2024-05-10 22:48:55 +0300

    The modal window code has been moved to js, ​​the index.js file has been renamed as welcome.js, a convenient anchor button has been added for all main pages

--- scripts/global/main.js
+++ scripts/global/main.js
@@ -1,3 +1,4 @@
+//ADAPTIVE HEADER
 const hamburgerBtn = document.querySelector(".hamburger__icon");
 
 hamburgerBtn.addEventListener("click", () => {
@@ -16,4 +17,24 @@ hamburgerBtn.addEventListener("click", () => {
       close.remove();
     }
   });
+});
+
+
+//scrollToTopBtn 
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Функция, которая показывает кнопку при прокрутке вниз
+window.addEventListener("scroll", () => {
+  // Если прокрутка больше чем 20 пикселей от верха страницы, показываем кнопку
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+// Функция, которая позволяет прокрутить страницу вверх при нажатии на кнопку
+scrollToTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0; // Для Safari
+  document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
 });
---
+++ scripts/pages/welcome.js
@@ -0,0 +1,47 @@
+let main = document.querySelector(".main");
+
+main.innerHTML += `  
+  <div class="modal">
+    <div class="modal__content">
+      <span class="content__close">&times;</span>
+      <h2>Login to account</h2>
+      <form class="content__form">
+        <label for="form__username">Login:</label>
+        <input type="text" class="form__username" name="username" required>
+        <label for="form__password">Password:</label>
+        <input type="password" class="form__password" name="password" required>
+        <input class="form__button" type="submit" value="Login">
+      </form>
+    </div>
+  </div>
+`;
+
+let openModalBtn = document.querySelector(".openModal");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".content__close");
+let form = document.querySelector(".content__form");
+
+openModalBtn.onclick = () => {
+  modal.style.display = "block";
+};
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let formLogin = document.querySelector(".form__username").value.trim();
+  let formPassword = document.querySelector(".form__password").value.trim();
+
+  if (formLogin && formPassword) {
+    window.location.href = "../../pages/home.html";
+  }
+});
